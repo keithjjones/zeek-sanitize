2d6d1b85cd6885a23268e35f726bb7809c2432fd
Keith Jones <keith@keithjjones.com>
2021-04-01 11:57:19 -0400

    Switch to an integer.

--- sanitize.py
+++ sanitize.py
@@ -2,19 +2,23 @@ import sys
 import csv
 import hashlib
 
-if len(sys.argv) != 3:
-    print("\nUsage:\n\t{0} <salt value> <field list, comma delimited>".format(sys.argv[0]))
+i = 1
+hash_values = dict()
+
+if len(sys.argv) != 2:
+    print("\nUsage:\n\t{0} <field list, comma delimited>".format(sys.argv[0]))
     print("\nExample:\n\tcat conn.log | zeek-cut | python {0} mysalt 2,4\n".format(sys.argv[0]))
     exit(-1)
 
-salt = sys.argv[1]
-fields = sys.argv[2].split(',')
+fields = sys.argv[1].split(',')
 
 rd = csv.reader(sys.stdin, delimiter="\t", quotechar='"')
 wt = csv.writer(sys.stdout, delimiter="\t", quotechar='"', quoting=csv.QUOTE_MINIMAL)
 for row in rd:
     for field in fields:
-        tmpstr = "{0}{1}".format(salt, row[int(field)])
-        row[int(field)] = hashlib.sha512(tmpstr.encode('utf-8')).hexdigest()
-        row[int(field)] = "REDACTED-{0}".format(row[int(field)][-16:])
+        hash_val = hashlib.md5(row[int(field)].encode('utf-8')).hexdigest()
+        if hash_val not in hash_values:
+            hash_values[hash_val] = i
+            i += 1
+        row[int(field)] = "REDACTED-{0}".format(hash_values[hash_val])
     wt.writerow(row)
